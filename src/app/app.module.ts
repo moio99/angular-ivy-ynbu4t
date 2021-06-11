d3a843b36edcdb47e6d8ec976ced962741e94847
moio99 <moio99@gmail.com>
2021-06-11 12:07:40 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-//import { SquareCellComponent } from './square-cell/square-cell.component';
-import { SquareCellComponent } from './squarecell/squarecell.component';
+import { SquareCellComponent } from './square-cell/square-cell.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent /* , SquareCellComponent */],
+  declarations: [AppComponent, SquareCellComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
